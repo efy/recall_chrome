13257df54c5964ba8d780a42ac5d1c3c29404573
Shaun Dowling <shaun.dowling@gmail.com>
2016-10-14 22:06:17 +0100

    Add keyboard shortcut and handler

--- background.js
+++ background.js
@@ -1,11 +1,23 @@
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
   suggest([
     {content: "Hackernews", description: "<url>http://news.ycombinator.com</url> <dim>Hackernews</dim>"},
-    {content: "Designernews", description: "<url>http://designernews.com</url> <dim>Designer News</dim>"}
+    {content: "Designernews", description: "<url>http://designernews.com</url> <dim>Designer News</dim>"},
     {content: "It's Nice That", description: "<url>http://itsnicethat.com</url> <dim>It's Nice That</dim>"}
   ])
 })
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
   alert('You just typed "' + text + '"')
+})
+
+chrome.commands.onCommand.addListener(function(command) {
+  if(command === "bookmark_current_page") {
+    chrome.tabs.query({active: true}, function(tabs) {
+      var tab = tabs[0]
+      var bookmark = {
+        url: tab.url,
+        title: tab.title
+      }
+    })
+  }
 })
