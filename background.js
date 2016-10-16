c5ec28910321184e956ad84fab1e460178e950e0
Shaun Dowling <shaun.dowling@gmail.com>
2016-10-16 21:49:48 +0100

    Add posting bookmarks to configured server

--- background.js
+++ background.js
@@ -18,6 +18,34 @@ chrome.commands.onCommand.addListener(function(command) {
         url: tab.url,
         title: tab.title
       }
+
+      post_bookmark(bookmark)
     })
   }
 })
+
+function post_bookmark(bookmark) {
+  var data = new FormData()
+  var xhr = new XMLHttpRequest()
+
+  chrome.storage.sync.get({
+    server_address: 'http://localhost'
+  }, function(items) {
+    var endpoint = items.server_address + '/api/v0/bookmarks'
+    data.append('url', bookmark.url)
+    data.append('title', bookmark.title)
+
+    xhr.open('POST', endpoint, true)
+    xhr.load = function() {
+      if(xhr.readyState === 4 && http.status === 200) {
+        console.log(xhr.responseText)
+      }
+    }
+
+    xhr.onerror = function() {
+      console.log(xhr)
+    }
+
+    xhr.send(data)
+  })
+}
