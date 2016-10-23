3631e543e697700879e36a3e1a47916b52b428c7
Shaun Dowling <shaun.dowling@gmail.com>
2016-10-23 02:23:19 +0100

    Add method for retrieving favicon

--- background.js
+++ background.js
@@ -14,12 +14,17 @@ chrome.commands.onCommand.addListener(function(command) {
   if(command === "bookmark_current_page") {
     chrome.tabs.query({active: true}, function(tabs) {
       var tab = tabs[0]
-      var bookmark = {
-        url: tab.url,
-        title: tab.title
-      }
+      fetch_favicon(tab.url).then(function(favicon){
+        var bookmark = {
+          url: tab.url,
+          title: tab.title,
+          icon: favicon
+        }
 
-      post_bookmark(bookmark)
+        console.log(bookmark);
+
+        post_bookmark(bookmark)
+      })
     })
   }
 })
@@ -34,6 +39,7 @@ function post_bookmark(bookmark) {
     var endpoint = items.server_address + '/api/v0/bookmarks'
     data.append('url', bookmark.url)
     data.append('title', bookmark.title)
+    data.append('icon', bookmark.icon)
 
     xhr.open('POST', endpoint, true)
     xhr.onload = function() {
@@ -58,5 +64,23 @@ function post_success_notification(bookmark) {
     iconUrl: 'recall_icon.png'
   }
   chrome.notifications.create(opts)
+}
+
+function fetch_favicon(url) {
+  return new Promise(function(resolve, reject) {
+    var img = new Image()
+    img.onload = function () {
+        var canvas = document.createElement("canvas")
+        canvas.width =this.width
+        canvas.height =this.height
+
+        var ctx = canvas.getContext("2d")
+        ctx.drawImage(this, 0, 0)
+
+        var dataURL = canvas.toDataURL("image/png")
+        resolve(dataURL)
+    }
+    img.src = 'chrome://favicon/' + url
+  })
 }
 
