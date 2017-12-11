3949e4261f73454f53355403ef35aae3eb2ace63
Shaun Dowling <shaun.dowling@gmail.com>
2017-12-11 23:50:21 +0000

    add drag handling

--- inject.js
+++ inject.js
@@ -1,10 +1,51 @@
 
 (function() {
-  // Show unobtrusive ui when dragging an item on
+  // Shows unobtrusive ui when dragging an item on
   // the current page
+  var b = document.body
+  var dropzone = dropzone_tmpl()
+
   document.body.addEventListener('dragstart', function(e) {
-    console.log("drag start")
+    b.appendChild(dropzone)
   })
 
-  console.log("hello from injected script")
+  document.body.addEventListener('dragend', function(e) {
+    b.removeChild(dropzone)
+  })
+
+  function dropzone_tmpl() {
+    var el = document.createElement('div')
+    el.id = "rc-dropzone"
+    el.classList.add('rc-dropzone')
+    el.textContent = "Add to recall"
+
+    el.addEventListener('drop', function(e){
+      e.preventDefault()
+
+      if (e.dataTransfer.types != null) {
+        var types = Array.from(e.dataTransfer.types)
+        types.forEach(function(t){
+          console.log(t)
+        })
+      }
+
+      if (e.dataTransfer.items != null) {
+        var items = Array.from(e.dataTransfer.items)
+        items.forEach(function(item){
+          console.log(item.kind, item.type)
+        })
+      }
+    })
+
+    el.addEventListener('dragover', function(e){
+      e.preventDefault()
+      el.classList.add("rc-dropzone__active")
+    })
+
+    el.addEventListener('dragleave', function(e){
+      el.classList.remove("rc-dropzone__active")
+    })
+
+    return el
+  }
 })();
